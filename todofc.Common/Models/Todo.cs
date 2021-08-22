3ef406a437ba0bbcbd7d78104bb360eeefa2b3a6
frank-kmiItm <88784452+frank-kmiItm@users.noreply.github.com>
2021-08-22 13:41:02 -0500

    Add entities

---
+++ todofc.Common/Models/Todo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace todofc.Common.Models
+{
+    public class Todo
+    {
+        public DateTime CreatedTime { get; set; }
+        public string TaskDescription { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
---
+++ todofc.Functions/Entities/TotoEntity.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace todofc.Functions.Entities
+{
+    public class TotoEntity : TableEntity
+    {
+        public DateTime CreatedTime { get; set; }
+        public string TaskDescription { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
